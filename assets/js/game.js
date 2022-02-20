c17f2b4c2cd0462834357e470a757506d1b261df
RevChrisWarren <chriswarren@Chriss-Mac-mini.attlocal.net>
2022-02-19 18:26:44 -0600

    added array and for statements

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,11 +3,22 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames[1])
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
 
-var fight = function() {
+
+var fight = function(enemyName) {
     //Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -65,4 +76,6 @@ else if (promptFight === "skip" || promptFight ==="SKIP") {
       window.alert("You need to choose a valid option. Try Again!");
   }
 }
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
