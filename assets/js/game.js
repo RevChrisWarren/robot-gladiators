dc6089dd79f05115e24767632498e45d7444a179
RevChrisWarren <chriswarren@Chriss-Mac-mini.attlocal.net>
2022-02-21 18:07:27 -0600

    High Score Featire Added

--- assets/js/game.js
+++ assets/js/game.js
@@ -140,12 +140,20 @@ endGame();
 
 var endGame = function() {
     window.alert("The game has now ended. let's see how you did");
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
-    }    else {
-        window.alert("You've lost your robot in battle!");
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
     }
+    //if player has more money than the high score, player has new high score
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highScore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
 
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money +"!");
+    }
+    else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + " . Maybe next time!");
+    }
 
 var playAgainConfirm = window.confirm("Would you like to play again?");
 
